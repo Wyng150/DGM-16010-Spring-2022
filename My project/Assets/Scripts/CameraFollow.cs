935a8760e79c84686726221d966a85f9fe9f763e
Russell NIelsen <Drawnbyruss@gmail.com>
2022-02-05 21:54:36 -0700

    camera follow/ gundam wing

--- My project/Assets/Scripts/CameraFollow.cs
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 2, -10);
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offset ;
     }
 }
