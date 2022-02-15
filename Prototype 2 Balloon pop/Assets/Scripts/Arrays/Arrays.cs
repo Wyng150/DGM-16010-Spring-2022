bdfb09b4d31889683db381d75969723e961225d9
Russell NIelsen <Drawnbyruss@gmail.com>
2022-02-14 17:05:29 -0700

    Array practice

---
+++ Prototype 2 Balloon pop/Assets/Scripts/Arrays/Arrays.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    public string[] friendNames = {"Mike", "Chris", "Steve", "Quigs", "Collin", "Jeppo", "Janelle", "Jordan"};
+    public int[] numbers = {1, 15, 69, 42, 100, 109938533, 234,};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        /*Debug.Log(friendNames[0]);
+        friendNames;  */
+        foreach(int numbers in numbers)
+      
+        {
+            print(numbers);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
