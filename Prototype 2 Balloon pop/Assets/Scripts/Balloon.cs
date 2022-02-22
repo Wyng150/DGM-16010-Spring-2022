c2e5c1192477a16953b5cd425f6972c2a583918a
Russell NIelsen <Drawnbyruss@gmail.com>
2022-02-21 19:37:49 -0700

    created scoreManager

--- Prototype 2 Balloon pop/Assets/Scripts/Balloon.cs
+++ Prototype 2 Balloon pop/Assets/Scripts/Balloon.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Balloon : MonoBehaviour
 {
     public int scoreToGive = 100; //score for popped balloon
-    public int clickToPop = 6; //clicks until balloon pops
+    public int clickToPop = 3; //clicks until balloon pops
     public float inflation = 0.10f;
+    public ScoreManager scoreManager; // a variable to reference score manager script
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,7 @@ public class Balloon : MonoBehaviour
 
         if(clickToPop == 0);
         {
+            scoreManager.IncreaseScoreText(scoreToGive);
             Destroy(gameObject);
         }
         
---
+++ Prototype 2 Balloon pop/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score; //keep track of score
+    public TextMeshProUGUI scoreText; // txt object to be modified
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IncreaseScoreText(int amount)
+    {
+        score += amount;
+        UpdateScoreText();
+    }
+    public void UpdateScoreText()
+    {
+        scoreText.text = "score: "+ score;
+    }
+}
