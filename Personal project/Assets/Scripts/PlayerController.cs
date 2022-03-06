46be9d2afdd22b36952d1cf1ba1ed8df8a702e43
Russell NIelsen <Drawnbyruss@gmail.com>
2022-03-06 15:51:40 -0700

    created personal project

---
+++ Personal project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = getComponent<Rigidbody2D>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveSpeed *Time.deltaTime);
+    }
+}
