92e1761cbb02e17d2fcaa58c57318f3397890364
Russell NIelsen <Drawnbyruss@gmail.com>
2022-03-23 16:36:35 -0600

    Patrol Script

---
+++ Rogue like/Assets/Scripts/Patrol.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float starWaitTime;
+    public Transform StartPoint;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = starWaitTime;
+        StartPoint.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, StartPoint.position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, StartPoint.position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                StartPoint.position = new Vector2(Random.Range(minX,maxX), Random.Range(minY,maxY));
+                waitTime = starWaitTime;
+            }
+            else
+            {
+                waitTime = Time.deltaTime;
+            }
+        }
+    }
+}
--- Rogue like/Assets/Scripts/Projectile.cs
+++ Rogue like/Assets/Scripts/Projectile.cs
@@ -18,12 +18,13 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
         if(transform.position.x == target.x && transform.position.y == target.y)
         {
             DestroyProjectile();
         }
     }
-    void OnTriggerEnter(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
